90beaf24bd40b8fdf7aa0fc3c889561c96c9da26
Strakaman <straka3000@gmail.com>
2014-09-15 17:00:51 -0700

    Created basic controller script

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CheckInputs();
+	
+	}
+
+	/**
+	 * Used for all player button inputs...I guess...
+	 */
+	void CheckInputs()
+	{
+		if (Input.GetButtonDown("Fire Spell"))
+		{
+			Debug.Log("Mystical Fire Spell, I summon thee!");
+		}
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0));
+	}
+}
