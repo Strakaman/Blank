15b0a205007ded1fbeabf2a346f294f0634ee8cf
Strakaman <straka3000@gmail.com>
2014-09-16 01:34:41 -0700

    Added 75% of peter's test code

--- Assets/PlayerControllerScript.cs
+++ Assets/PlayerControllerScript.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class PlayerControllerScript : MonoBehaviour {
 	public GameObject refBullet;
+	public Animator animator;
+	public const float speed = 10;
 	// Use this for initialization
 	void Start () {
-
+		animator = (Animator)GetComponent("Animator");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		CheckInputs();
-	
 	}
 
 	/**
@@ -20,14 +20,21 @@ public class PlayerControllerScript : MonoBehaviour {
 	 */
 	void CheckInputs()
 	{
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0)); //O: better to create horizontal vector once?
+		transform.Translate(new Vector3(0,Input.GetAxis("Vertical")*.05f,0));   //O: better to create horizontal vector once?
 		if (Input.GetButtonDown("Fire Spell"))
 		{
-			Debug.Log("Mystical Fire Spell, I summon thee!");
-			GameObject clonedesu = (GameObject)Instantiate(refBullet, transform.position,transform.rotation);
-			//clonedesu.rigidbody2D.velocity = new Vector3(0,2,0);
-			BroadcastMessage("ApplyVelocity",clonedesu);
+			FireSpell();
 		}
+	}
 
-		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0));
+	/**
+	 * Offensive spell for player 
+	 */
+	void FireSpell()
+	{
+		GameObject clonedesu = (GameObject)Instantiate(refBullet, transform.position,transform.rotation);
+		clonedesu.rigidbody2D.velocity = new Vector3(speed,0,0);
+		Destroy(clonedesu, 3);
 	}
 }
