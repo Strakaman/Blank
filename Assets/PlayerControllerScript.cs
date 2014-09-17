70e4f45e958eeeefcc5ede8fd15c27d9d5743015
Peter Pham <phamanpeter@gmail.com>
2014-09-17 12:58:36 -0700

    Fixed Animation Bugs + Added Bounce Projectile (F)

--- Assets/PlayerControllerScript.cs
+++ Assets/PlayerControllerScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerControllerScript : MonoBehaviour {
 	public GameObject refBullet;
+	public GameObject refBullet2;
+
 	public Animator animator;
 	public const float speed = 10;
 	// Use this for initialization
@@ -18,10 +20,10 @@ public class PlayerControllerScript : MonoBehaviour {
 
 	void SpriteAnimation() {
 		if (rigidbody2D.velocity.x != 0 || rigidbody2D.velocity.y != 0) {
-						animator.SetBool ("doWalk", true);
-				} else {
-			animator.SetBool ("doWalk", false);
-			  }
+			animator.SetBool ("doWalk", true);
+		} else {
+		animator.SetBool ("doWalk", false);
+		}
 	}
 	/**
 	 * Used for all player button inputs...I guess...
@@ -30,10 +32,14 @@ public class PlayerControllerScript : MonoBehaviour {
 	{
 		//transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0)); //O: better to create horizontal vector once?
 		//transform.Translate(new Vector3(0,Input.GetAxis("Vertical")*.05f,0));   //O: better to create horizontal vector once?
-		rigidbody2D.velocity = new Vector2(Input.GetAxis ("Horizontal") * speed, 0);
+		rigidbody2D.velocity = new Vector2(Input.GetAxis ("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
 		if (Input.GetButtonDown("Fire Spell"))
 		{
 			FireSpell();
+		}
+		if (Input.GetButtonDown("Bounce Spell"))
+		{
+			BounceSpell();
 		}
 	}
 
@@ -43,6 +49,12 @@ public class PlayerControllerScript : MonoBehaviour {
 	void FireSpell()
 	{
 		GameObject clonedesu = (GameObject)Instantiate(refBullet, transform.position,transform.rotation);
+		clonedesu.rigidbody2D.velocity = new Vector3(speed,0,0);
+		Destroy(clonedesu, 3);
+	}
+
+	void BounceSpell() {
+		GameObject clonedesu = (GameObject)Instantiate(refBullet2, transform.position,transform.rotation);
 		clonedesu.rigidbody2D.velocity = new Vector3(speed,0,0);
 		Destroy(clonedesu, 3);
 	}
