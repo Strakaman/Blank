6006a8cb73e773967bb080c172ae412151675128
Nghia Pham <phamqnghia@gmail.com>
2014-09-17 10:08:13 -0700

    Walk animations
    added walk animations during cs club.

--- Assets/PlayerControllerScript.cs
+++ Assets/PlayerControllerScript.cs
@@ -13,15 +13,24 @@ public class PlayerControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		CheckInputs();
+		SpriteAnimation ();
 	}
 
+	void SpriteAnimation() {
+		if (rigidbody2D.velocity.x != 0 || rigidbody2D.velocity.y != 0) {
+						animator.SetBool ("doWalk", true);
+				} else {
+			animator.SetBool ("doWalk", false);
+			  }
+	}
 	/**
 	 * Used for all player button inputs...I guess...
 	 */
 	void CheckInputs()
 	{
-		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0)); //O: better to create horizontal vector once?
-		transform.Translate(new Vector3(0,Input.GetAxis("Vertical")*.05f,0));   //O: better to create horizontal vector once?
+		//transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0)); //O: better to create horizontal vector once?
+		//transform.Translate(new Vector3(0,Input.GetAxis("Vertical")*.05f,0));   //O: better to create horizontal vector once?
+		rigidbody2D.velocity = new Vector2(Input.GetAxis ("Horizontal") * speed, 0);
 		if (Input.GetButtonDown("Fire Spell"))
 		{
 			FireSpell();
