5c575acb480544894c41f67e985ea3654b368276
Strakaman <straka3000@gmail.com>
2014-09-17 15:59:12 -0700

    Revert "broke sumthing oops"
    This reverts commit ea2ac3e48c53407f36be48e14b145766288f9a67.

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public GameObject refBullet;
+	public Animator animator;
+	public const float speed = 10;
+	// Use this for initialization
+	void Start () {
+		animator = (Animator)GetComponent("Animator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInputs();
+	}
+
+	/**
+	 * Used for all player button inputs...I guess...
+	 */
+	void CheckInputs()
+	{
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal")*.05f,0,0)); //O: better to create horizontal vector once?
+		transform.Translate(new Vector3(0,Input.GetAxis("Vertical")*.05f,0));   //O: better to create horizontal vector once?
+		if (Input.GetButtonDown("Fire Spell"))
+		{
+			FireSpell();
+		}
+	}
+
+	/**
+	 * Offensive spell for player 
+	 */
+	void FireSpell()
+	{
+		GameObject clonedesu = (GameObject)Instantiate(refBullet, transform.position,transform.rotation);
+		clonedesu.rigidbody2D.velocity = new Vector3(speed,0,0);
+		Destroy(clonedesu, 3);
+	}
+}
