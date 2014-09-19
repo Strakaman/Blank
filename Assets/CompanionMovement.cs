228217854771e041ef5caa8ec0bd04c4880795ba
Pforpeetar <phamanpeter@gmail.com>
2014-09-19 11:36:56 -0700

    Surprise!!!
    Spin to win.

--- Assets/CompanionMovement.cs
+++ Assets/CompanionMovement.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class CompanionMovement : MonoBehaviour {
 	public Transform target;
 	public int degrees = 1;
+
+	void Start() {
+		Vector3 orbit = new Vector3(target.position.x + 1, target.position.y + 1, target.position.z);
+		this.transform.position = orbit;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = target.transform.position * 2;
-		transform.RotateAround (target.position, Vector3.forward, degrees);
+		//this.transform.position = target.transform.position * 2;
+		transform.RotateAround (target.transform.position, Vector3.forward, degrees);
 	}
 }
