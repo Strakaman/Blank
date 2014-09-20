8c2dff8c8c8e8a1d0120755945c7636d6f27f3bb
Pforpeetar <phamanpeter@gmail.com>
2014-09-19 22:48:42 -0700

    Bugged Camera

--- Assets/CameraPan.cs
+++ Assets/CameraPan.cs
@@ -3,12 +3,50 @@ using System.Collections;
 
 public class CameraPan : MonoBehaviour {
 	public GameObject player;
-	public Camera mainCam;
+	public int numInvoke = 0;
+	public int i;
+	public int j;
+
 	// Update is called once per frame
+
+
 	void FixedUpdate () {
-		//if (player.transform.position == mainCam.ScreenToWorldPoint
-		//Vector2 pan = new Vector2 (transform.position.x+1, transform.position.y);
-		//transform.position = pan;	
+		Vector3 trim = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
+		Vector3 pos = Camera.main.WorldToViewportPoint(trim);
+		
+		if (pos.x < 0.0) {
+			//transform.position = Vector3.Lerp(transform.position, pan, Time.smoothDeltaTimes);
+			//transform.position = Vector3.MoveTowards(transform.position, pan, 1);
+			i = -10;
+			j = 0;
+			InvokeRepeating("pan", 0.25f, 0.25F);
+			numInvoke = 0;
+		}
+		if (1.0 < pos.x) {
+			i = 10;
+			j = 0;
+			InvokeRepeating("pan", 0.25f, 0.25F);
+			numInvoke = 0;
+		}
+		if (pos.y < 0.0) {
+			i = 0;
+			j = -10;
+			InvokeRepeating("pan", 0.25f, 0.25F);
+			numInvoke = 0;
+		}
+		if (1.0 < pos.y) {
+			i = 0;
+			j = +10;
+			InvokeRepeating("pan", 0.25f, 0.25F);
+			numInvoke = 0;	
+		}
 	}
-	
+
+	void pan() {
+		if (numInvoke < 20) {
+				numInvoke++;
+				Vector3 pan = new Vector3 (transform.position.x + i, transform.position.y + j, -10);
+				transform.position = Vector3.MoveTowards (transform.position, pan, .5f);
+		}
+	}
 }
--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -5,17 +5,32 @@ public class Orbit : MonoBehaviour {
 	public Transform target;
 	public int degrees = 1;
 	public float radius = 1;
-	public int numOrbits = 1;
 	public bool orbitDir = true;
+	public int startPosition = 0;
 
 	void Start() {
-		Vector3 orbit = new Vector3(target.position.x + radius, target.position.y + radius, target.position.z);
-		this.transform.position = orbit;
+		if (startPosition == 0) {
+			Vector3 orbit = new Vector3 (target.position.x + radius, target.position.y + radius, target.position.z);
+			this.transform.position = orbit;
+		} else if (startPosition == 1) {
+			Vector3 orbit = new Vector3 (target.position.x + radius, target.position.y + -radius, target.position.z);
+			this.transform.position = orbit;
+		} else if (startPosition == 2) {
+			Vector3 orbit = new Vector3 (target.position.x + -radius, target.position.y + radius, target.position.z);
+			this.transform.position = orbit;
+		} else if (startPosition == 3) {
+			Vector3 orbit = new Vector3 (target.position.x + -radius, target.position.y + -radius, target.position.z);
+			this.transform.position = orbit;
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		//this.transform.position = target.transform.position * 2;
-		transform.RotateAround (target.transform.position, Vector3.forward, degrees);
+		if (orbitDir == true) {
+			transform.RotateAround (target.transform.position, Vector3.forward, degrees);
+		} else {
+			transform.RotateAround (target.transform.position, Vector3.back, degrees);
+		}
 	}
 }
