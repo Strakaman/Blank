a0b7adfe6aba014992fa6cb9bcb4b6a8344e3d5b
Strakaman <straka3000@gmail.com>
2014-09-20 20:18:57 -0700

    Added metadata for player
    PlayerInfo class is static and does not need to be initialized
    Change Health method for healing/damaging player
    ChangeMana method for increasing/decreasing mana

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerInfo : MonoBehaviour {
+public static class PlayerInfo 
+{
+	private const int MAXMANA = 300;
+	private const int MAXHEALTH = 100;
+	private static int mana = MAXMANA;
+	private static int health = MAXHEALTH;
 
-	// Use this for initialization
-	void Start () {
-	
+	public static int getHealth()
+	{
+		return health;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public static int getMana()
+	{
+		return mana;
+	}
+
+	public static void resetPlayer()
+	{
+		mana = MAXMANA;
+		health = MAXHEALTH;
+	}
+
+	public static void changeHealth(int byHowMuchHealth)
+	{
+		health += byHowMuchHealth;
+		if (health > MAXHEALTH)
+		{
+			health = MAXHEALTH;
+		}
+		if (health <= 0)
+		{
+			health = 0;
+			//TODO: kill player workflow
+		}
+	}
+
+	public static void changeMana(int byHowMuchMana)
+	{
+		mana += byHowMuchMana;
+		if (mana > MAXMANA)
+		{
+			mana = MAXMANA;
+		}
+		if (mana < 0)
+		{
+			mana = 0;
+		}
 	}
 }
