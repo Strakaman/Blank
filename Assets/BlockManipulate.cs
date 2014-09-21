825ba3bf0454a84def49e7e6be9c3891edb619c6
Pforpeetar <phamanpeter@gmail.com>
2014-09-20 22:24:32 -0700

    679

---
+++ Assets/BlockManipulate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockManipulate : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D coll) {
+				while (coll.gameObject.tag == "Player") {
+				Debug.Log("Colliding");
+						if (Input.GetButtonDown("Grab")) {
+						Debug.Log("Grabbing");
+								transform.parent = coll.gameObject.transform;
+						}
+				}
+		}
+}
