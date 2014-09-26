4b538364804f3cf89367d128db1c19fb17b0dff4
jaydayy77 <jmday1996@gmail.com>
2014-09-25 19:40:32 -0700

    Main menu stuff desu

---
+++ Assets/Main Menu/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	public float guiPlacementY;
+
+	void OnGUI(){
+
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),backgroundTexture);
+
+		if (GUI.Button (new Rect(Screen.width * .5f, Screen.height * .4f, Screen.width * .5f, Screen.height * .1f), "Play Game")){
+			print ("Clicked Play Game");
+		}
+
+		if (GUI.Button (new Rect (Screen.width * .5f, Screen.height * .55f, Screen.width * .5f, Screen.height * .1f), "End Game")) {
+						print ("Clicked End Game");
+	
+		}
+	}
+}
