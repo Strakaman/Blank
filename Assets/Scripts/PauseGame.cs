a03c5c3cc90c2174b0967b4e07d1cd09b744bd8e
Dennis <denniseh7@gmail.com>
2014-09-25 19:27:15 -0700

    added-pause menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PauseGame : MonoBehaviour {
+	 bool paused = false;
+
+	void Update()
+	{
+		if (Input.GetButtonDown ("pause"))
+		{
+			paused = togglePause ();
+
+		}
+
+		if (paused) 
+		{
+			Time.timeScale = 0f;
+			guiTexture.enabled = true;
+			Debug.Log("Game is Paused");
+
+		}
+		else
+		{
+			Time.timeScale = 1f;
+			guiTexture.enabled = false;
+			Debug.Log("Game is Resumed");
+		}
+	}
+
+	bool togglePause()
+	{
+		if (paused == true) 
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+				
+	}
+}
