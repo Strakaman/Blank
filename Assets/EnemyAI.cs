c2fef0b11f2d46657eff329ee451cfa4842c5940
Pforpeetar <phamanpeter@gmail.com>
2014-09-29 00:32:44 -0700

    Basic Enemy Follow
    kushing the push

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	private Vector3 Player;	
+	private Vector2 Playerdirection;
+	private float Xdif;
+	private float Ydif;
+	public float speed;
+	
+	void Update () {
+		Player = GameObject.FindGameObjectWithTag ("Player").transform.position;
+
+		Xdif = Player.x - transform.position.x;
+		Ydif = Player.y - transform.position.y;
+
+		Playerdirection = new Vector2 (Xdif, Ydif);
+
+		rigidbody2D.velocity = (Playerdirection.normalized * speed);
+	}
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour{
 	private Animator animator;
 	private string enemyName;
 	private int redResis;
+	public Direction direction = 0; //0 is down, 1 is up, 2 is left, 3 is right
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,75 @@ public class Enemy : MonoBehaviour{
 			Destroy(gameObject, 1);
 			gameObject.collider2D.enabled = false;
 		}
+		if (Time.timeScale!=0) 
+		{
+			SpriteAnimation ();
+		}
+	}
+
+	
+	//Checks and sets the animation state for the player
+	void SpriteAnimation ()
+	{
+		SetDirection ();
+		setWalk ();
+		setIdle ();
+	}
+	
+	
+	
+	//Sets the direction for the player
+	void SetDirection ()
+	{
+		if (rigidbody2D.velocity.x < -1) {
+			direction = Direction.left;
+		} else if (rigidbody2D.velocity.x > 1) {
+			direction = Direction.right;
+		} else if (rigidbody2D.velocity.y < 0) {
+			direction = Direction.down;
+		} else if (rigidbody2D.velocity.y > 0) {
+			direction = Direction.up;
+		}
+	}
+	
+	//Sets the walking animation for the player if they are moving
+	void setWalk ()
+	{
+		if (rigidbody2D.velocity.x != 0 || rigidbody2D.velocity.y != 0) {
+			if (direction == Direction.down) {
+				SetBools (true, false, false, false);
+			} else if (direction == Direction.up) {
+				SetBools (false, true, false, false);
+			} else if (direction == Direction.left) {
+				SetBools (false, false, true, false);
+			} else if (direction == Direction.right) {
+				SetBools (false, false, false, true);
+			}
+		}
+	}
+	
+	//Sets the idle animation for the player if velocities are 0
+	void setIdle ()
+	{
+		if (rigidbody2D.velocity.x == 0 && rigidbody2D.velocity.y == 0) {
+			if (direction == Direction.down) {
+				animator.SetBool ("Down", false);
+			} else if (direction == Direction.up) {
+				animator.SetBool ("Top", false);
+			} else if (direction == Direction.left) {
+				animator.SetBool ("Left", false);
+			} else if (direction == Direction.right) {
+				animator.SetBool ("Right", false);
+			}
+		}
+	}
+	
+	public void SetBools (bool down, bool up, bool left, bool right)
+	{
+		animator.SetBool ("Down", down);
+		animator.SetBool ("Top", up);
+		animator.SetBool ("Left", left);
+		animator.SetBool ("Right", right);
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
