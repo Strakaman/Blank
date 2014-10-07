a990c2b81d7c1534871498acad1131ce70a1463a
Strakaman <straka3000@gmail.com>
2014-10-07 14:42:44 -0700

    Added Multi tag script to all objects w/ colliders

---
+++ Assets/Scripts/MultiTagScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiTagScript : MonoBehaviour {
+
+	public string[] objectTags;
+	public static string PLAYER = "Player";
+	public bool objectHasTag(string tagToCheckFor)
+	{
+		if ((tagToCheckFor == null)||(objectTags.Length == 0)) {return false;}
+		foreach (string curTag in objectTags) {
+			if(curTag.Equals(tagToCheckFor))
+			   {
+
+				return true;
+			}
+			}
+				
+				
+
+			
+			return false;
+		}
+}
