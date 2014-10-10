1c1daa12194bfd9e4b1dfc74016ffe3e7cf802f6
Wilson <wiloigal@gmail.com>
2014-10-09 21:24:26 -0700

    New HUD - causing scene to not compile - about to revert

--- Assets/Scripts/HUDNew.cs
+++ Assets/Scripts/HUDNew.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class HUDNew : MonoBehaviour {
 	public GUISkin theSkin;	
 	
-	var BGTexture: Texture2D;
-	BGTexture.setPixel (0,0, new Color (0,0,0,0.5));
+	Texture2D BGTexture;
+
 	
 	//var infoFont : Font;
 	
-	var HPTexture: Texture2D;
+	Texture2D HPTexture;
 	
 	
 	
@@ -25,7 +25,9 @@ public class HUDNew : MonoBehaviour {
 	
 	void OnGUI() {
 		GUI.skin = theSkin;
-		GUI.Box (new Rect ((1.0/6)Screen.width, (1.0/12)Screen.height, (2.0/3)Screen.width, (1.0/6)Screen.height), BGTexture);
+		//BGTexture.setPixel (0,0, new Color (0,0,0,0.5));
+		GUI.backgroundColor = new Color (0, 0, 0, 0.5);
+		GUI.Box (new Rect ((1.0/6)*Screen.width, (1.0/12)*Screen.height, (2.0/3)*Screen.width, (1.0/6)*Screen.height), BGTexture);
 		
 		GUI.Label (new Rect (50, 25, 100, 100), "Health: " + PlayerInfo.getHealth());
 		//GUI.Box (new Rect ((1.0/6)Screen.width, (1.0/12)Screen.height, (2.0/3)Screen.width, (1.0/6)Screen.height), BGTexture);
