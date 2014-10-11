3793b86808ca91ec151f87b93c49bb96793460c9
Pforpeetar <phamanpeter@gmail.com>
2014-10-10 19:01:12 -0700

    Revert "random stuff"
    This reverts commit ed17e09f1c17f9e6d0dbcba1a11d24b2b659f039.

--- Assets/Scripts/HUDNew.cs
+++ Assets/Scripts/HUDNew.cs
@@ -23,7 +23,7 @@ public class HUDNew : MonoBehaviour {
 		
 	}
 	
-	/*void OnGUI() {
+	void OnGUI() {
 		GUI.skin = theSkin;
 		//BGTexture.setPixel (0,0, new Color (0,0,0,0.5));
 		GUI.backgroundColor = new Color (0, 0, 0, 0.5);
@@ -33,5 +33,5 @@ public class HUDNew : MonoBehaviour {
 		//GUI.Box (new Rect ((1.0/6)Screen.width, (1.0/12)Screen.height, (2.0/3)Screen.width, (1.0/6)Screen.height), BGTexture);
 		GUI.Label (new Rect (Screen.width - 100, 25, 100, 100), "Mana: " + PlayerInfo.getMana());
 		GUI.Label (new Rect (50, 50, 100, 100), "Current Spell: " + PlayerInfo.getCurrSpell().getName());
-	}*/
+	}
 }
