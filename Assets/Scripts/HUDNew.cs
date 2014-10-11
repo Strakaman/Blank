a3e30c41b413e9037f4dd517155f9008df397cab
Strakaman <straka3000@gmail.com>
2014-10-10 18:59:42 -0700

    temp test scene fix

--- Assets/Scripts/HUDNew.cs
+++ Assets/Scripts/HUDNew.cs
@@ -24,14 +24,14 @@ public class HUDNew : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		GUI.skin = theSkin;
+		/*GUI.skin = theSkin;
 		//BGTexture.setPixel (0,0, new Color (0,0,0,0.5));
-		GUI.backgroundColor = new Color (0, 0, 0, 0.5);
-		GUI.Box (new Rect ((1.0/6)*Screen.width, (1.0/12)*Screen.height, (2.0/3)*Screen.width, (1.0/6)*Screen.height), BGTexture);
+		GUI.backgroundColor = new Color (0, 0, 0, 0.5f);
+		GUI.Box (new Rect ((1.0f/6)*Screen.width, (1.0f/12)*Screen.height, (2.0f/3)*Screen.width, (1.0f/6)*Screen.height), BGTexture);
 		
 		GUI.Label (new Rect (50, 25, 100, 100), "Health: " + PlayerInfo.getHealth());
 		//GUI.Box (new Rect ((1.0/6)Screen.width, (1.0/12)Screen.height, (2.0/3)Screen.width, (1.0/6)Screen.height), BGTexture);
 		GUI.Label (new Rect (Screen.width - 100, 25, 100, 100), "Mana: " + PlayerInfo.getMana());
-		GUI.Label (new Rect (50, 50, 100, 100), "Current Spell: " + PlayerInfo.getCurrSpell().getName());
+		GUI.Label (new Rect (50, 50, 100, 100), "Current Spell: " + PlayerInfo.getCurrSpell().getName());*/
 	}
 }
