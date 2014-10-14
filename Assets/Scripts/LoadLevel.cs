5fee09f3e69023cffd9f0907bdaf32f02a54687f
Pforpeetar <phamanpeter@gmail.com>
2014-10-13 18:28:00 -0700

    New Build

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class LoadLevel : MonoBehaviour {
-
+	public string Level;
 	// Use this for initialization
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (Utilities.hasMatchingTag("Player",coll.gameObject)) {
-			Application.LoadLevel ("Omari Test Scene");
+			Application.LoadLevel (Level);
 		}
 	}
 }
