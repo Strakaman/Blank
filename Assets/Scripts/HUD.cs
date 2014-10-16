3207cf6068fcc7b439665ddc434c06de650fa72a
Pforpeetar <phamanpeter@gmail.com>
2014-10-15 20:31:25 -0700

    HUDSLAMMMM!!!!
    YEEEEEE

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -2,7 +2,24 @@
 using System.Collections;
 
 public class HUD : MonoBehaviour {
-	public GUISkin theSkin;
+	public Texture2D redSpell;
+	public Texture2D blueSpell;
+	public Texture2D yellowSpell;
+	public Texture2D orangeSpell;
+	public Texture2D hpBar;
+	public Texture2D mpBar;
+	public Texture2D hp;
+	public Texture2D mp;
+	public Rect hpPosition;
+	public Rect mpPosition;
+	public Rect hpContainerPosition;
+	public Rect mpContainerPosition;
+	public Rect spellPosition;
+	private float health;
+	private float healthScale;
+	private float mana;
+	private float manaScale;
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,13 +27,52 @@ public class HUD : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		health = PlayerInfo.getHealth ();
+		mana = PlayerInfo.getMana ();
+		healthScale = health / 100;
+		manaScale = mana / 300;
+	}
+
+	void barUpdate(Rect pos, Texture2D tex, float scale) {
+		GUI.DrawTexture (new Rect(Screen.width * pos.x, Screen.height * pos.y, Screen.width * (pos.width * scale), Screen.height * pos.height), tex);
 	}
 
 	void OnGUI() {
+		/*
 		GUI.skin = theSkin;
-				GUI.Label (new Rect (50, 25, 100, 100), "Health: " + PlayerInfo.getHealth());
-				GUI.Label (new Rect (Screen.width - 100, 25, 100, 100), "Mana: " + PlayerInfo.getMana());
-				GUI.Label (new Rect (50, 50, 100, 100), "Current Spell: " + PlayerInfo.getCurrSpell().getName());
+		GUI.Label (new Rect (50, 25, 100, 100), "Health: " + PlayerInfo.getHealth());
+		GUI.Label (new Rect (Screen.width - 100, 25, 100, 100), "Mana: " + PlayerInfo.getMana());
+		GUI.Label (new Rect (50, 50, 100, 100), "Current Spell: " + PlayerInfo.getCurrSpell().getName());
+		 */
+		drawBar (hpContainerPosition, hpBar);
+		drawBar (mpContainerPosition, mpBar);
+		barUpdate (hpPosition, hp, healthScale);
+		barUpdate (mpPosition, mp, manaScale);
+	//	Debug.Log (PlayerInfo.getCurrSpell ().GetType());
+		if (PlayerInfo.getCurrSpell ().GetType().ToString() == "RedSpell") {
+			drawSpell (redSpell);
 		}
+		if (PlayerInfo.getCurrSpell ().GetType().ToString() == "BlueSpell") {
+			drawSpell (blueSpell);
+		}
+		if (PlayerInfo.getCurrSpell ().GetType().ToString() == "YellowSpell") {
+			drawSpell (yellowSpell);
+		}
+		if (PlayerInfo.getCurrSpell ().GetType().ToString() == "OrangeSpell") {
+			drawSpell (orangeSpell);
+		}
+	}
+
+	void drawBar(Rect pos, Texture2D bar) {
+		GUI.DrawTexture (getScreenRect(pos), bar);
+	}
+	
+
+	void drawSpell(Texture2D spell) {
+		GUI.DrawTexture(getScreenRect(spellPosition), spell);
+	}
+
+	Rect getScreenRect(Rect pos) {
+		return new Rect (Screen.width * pos.x, Screen.height * pos.y, Screen.width * pos.width, Screen.height * pos.height);
+	}
 }
--- Assets/Scripts/Spell Scripts/OrangeSpell.cs
+++ Assets/Scripts/Spell Scripts/OrangeSpell.cs
@@ -1,1 +1,28 @@
+using UnityEngine;
+using System.Collections;
 
+public class OrangeSpell : Spell
+{
+	GameObject redRefObject;
+	//stupid constructor synthax required to make subclass derive from base class properly
+	
+	public void initializeSpell(string nameDesu, string descriptionDesu, int manaCostDesu, int projSpeedDesu, float animDuration) 
+	{
+		base.initializeSpell(nameDesu, descriptionDesu, manaCostDesu, animDuration);
+		redRefObject = GameObject.FindGameObjectWithTag("OrangeSpellObject");
+		projectileSpeed = projSpeedDesu;
+	}
+	
+	public override void cast(Direction dir)
+	{
+		/*
+		 * if manaCost < currentMana 
+		 *  subtract mana
+		 *  create projectile
+		 * else
+		 *  display on GUI (not enough mana)
+		 */
+		createProjectile (dir, redRefObject);
+	}
+	
+}
