3c99b95e7de6a3084866328292344e0daa84441c
Pforpeetar <phamanpeter@gmail.com>
2014-10-16 00:43:20 -0700

    CHAIINNNN LIGHTNINGGGGG...Kinda.

--- Assets/Scripts/BulletDesu.cs
+++ Assets/Scripts/BulletDesu.cs
@@ -13,12 +13,10 @@ public class BulletDesu : MonoBehaviour {
 	
 	}
 	void OnCollisionEnter2D(Collision2D collInfo) {
-	{
-		//Debug.Log(collInfo.gameObject.name);		
+		//Debug.Log(collInfo.gameObject.name);	
 		animator.SetBool ("Does Collide", true);
 		gameObject.collider2D.enabled = false;
 		rigidbody2D.velocity = new Vector2(0,0);
 		Destroy(gameObject,.5f);
-		}
 	}
 }
