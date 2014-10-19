ec92afc3aeeebeab9c107c804e4df97d1dc1adb2
Strakaman <straka3000@gmail.com>
2014-10-19 10:23:05 -0700

    took oiut a debug

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -71,7 +71,7 @@ public static class Utilities
 * Pass in a orientation vector so that the sprite can be rotated accordingly
 */ 
 		public static GameObject cloneObject (Direction direction, GameObject bulletToClone, Vector3 placetoCreate, Vector3 velocity, Quaternion orientation)
-		{		Debug.Log(bulletToClone);
+		{		
 				GameObject clonedesu = (GameObject)ScriptableObject.Instantiate (bulletToClone, placetoCreate, orientation);
 				if (clonedesu.rigidbody2D) {
 						clonedesu.rigidbody2D.velocity = velocity;
