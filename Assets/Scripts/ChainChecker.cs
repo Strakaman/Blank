1bb511ffcaae26547c447e94db6e98cf833275a5
Strakaman <straka3000@gmail.com>
2014-11-08 23:58:12 -0800

    wtf

---
+++ Assets/Scripts/ChainChecker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainChecker : MonoBehaviour {
+
+	private bool checkEnemyInRadius;
+	public GameObject parent;
+	// Use this for initialization
+	void Start () {
+		checkEnemyInRadius = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*void OnTriggerStay2D(Collider2D collInfo) {
+		//Debug.Log (Utilities.hasMatchingTag("Enemy",collInfo.gameObject));
+		if (checkEnemyInRadius == true && Utilities.hasMatchingTag("Enemy", collInfo.gameObject) && collInfo != colTarget) {
+			//Debug.Log ("CHAIN!");
+			
+			//gameObject.collider2D.enabled = true;
+			target = collInfo.gameObject;
+			projectileTrajectory(target);
+			Destroy (gameObject, .5f);
+		}
+	}*/
+}
