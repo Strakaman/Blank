885f39410cafe5b893fad807a5e58c6f4e86a46d
Pforpeetar <phamanpeter@gmail.com>
2014-11-08 18:54:41 -0800

    Damage Trap and Begginings of a vector push trap.

---
+++ Assets/VectorPush.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorPush : MonoBehaviour {
+	public int pushForce;
+	public Direction direction;
+
+
+	void OnTriggerEnter2D(Collision2D coll) {
+
+	}
+}
