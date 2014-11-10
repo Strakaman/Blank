81f1a4ef75c3eea20e55dc05a098c6d74aab95d1
Peter Pham <phamanpeter@gmail.com>
2014-11-10 14:25:12 -0800

    Added a Patrol Script and Tweaked Damage Trap
    Script will patrol object in a direction you set in the inspector.
    Damage Trap now has an OnTriggerStay so that if you set trigger to on in
    the collider, it will pass through the player but still deal damage.

---
+++ Assets/PatrolScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolScript : MonoBehaviour {
+	public Direction direction;
+	public int startTimer = 0;
+	public int maxTimer = 100;
+	public int speed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		startTimer += 1;
+		if (startTimer >= maxTimer) {
+			startTimer = 0;
+			if (direction == Direction.up) {
+				direction = Direction.down;
+				return;
+			} else if (direction == Direction.down){
+				direction = Direction.up;
+				return;
+			}
+			if (direction == Direction.right) {
+				direction = Direction.left;
+				return;
+			} else if (direction == Direction.left){
+				direction = Direction.right;
+				return;
+			}
+		}
+
+		//Up and Down
+		if (direction == Direction.up) {
+			rigidbody2D.velocity = new Vector2(0, speed);
+		}
+		if (direction == Direction.down) {
+			rigidbody2D.velocity = new Vector2(0, -speed);
+		}
+
+		//Left and Right
+		if (direction == Direction.right) {
+			rigidbody2D.velocity = new Vector2(speed, 0);
+		}
+		if (direction == Direction.left) {
+			rigidbody2D.velocity = new Vector2(-speed, 0);
+		}
+	}
+}
--- Assets/Scripts/DamageTrap.cs
+++ Assets/Scripts/DamageTrap.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class DamageTrap : Switchable {
-	public int damage;
+	public int damage = 10;
+	public int knockback = 1000;
+	public float hitDelay = 0.25f;
 	public bool isOn;
 	public Animator animator;
 
@@ -35,9 +37,18 @@ public class DamageTrap : Switchable {
 
 	void OnCollisionStay2D(Collision2D coll) {
 		if (Utilities.hasMatchingTag("Player",coll.gameObject)) {
-			DamageStruct laserStruct = new DamageStruct (damage, collider2D.gameObject, 1000, 0.25f); 
+			DamageStruct laserStruct = new DamageStruct (damage, collider2D.gameObject, knockback, hitDelay); 
 			//struct used to pass more than one parameter through send message, which only lets you pass one object as a parameter
 			coll.gameObject.SendMessage("callDamage", laserStruct);
 		}
 	}
+
+	void OnTriggerStay2D(Collider2D coll) {
+		if (Utilities.hasMatchingTag("Player",coll.gameObject)) {
+			DamageStruct laserStruct = new DamageStruct (damage, collider2D.gameObject, knockback, hitDelay); 
+			//struct used to pass more than one parameter through send message, which only lets you pass one object as a parameter
+			coll.gameObject.SendMessage("callDamage", laserStruct);
+		}
+	}
+
 }
