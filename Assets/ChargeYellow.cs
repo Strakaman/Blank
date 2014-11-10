3c3cf6bf2c69a2d11c6c6505548fd98e3c4fc31c
Strakaman <straka3000@gmail.com>
2014-11-10 00:35:44 -0800

    boom charge yellow
    all charges complete, hopefully it adds some fun

---
+++ Assets/ChargeYellow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeYellow : MonoBehaviour {
+
+	public int damage; //should be a low number since damage is applied during onTriggerStay
+	public string whoToStrike ;//Player or Enemy
+	// Use this for initialization
+	void Start () {
+		if (whoToStrike == null) {
+			whoToStrike = "Enemy";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D (Collider2D collInfo)
+	{//if meant to hit player, will hit player, if meant to hit enemy, will hit enemy
+		if (Utilities.hasMatchingTag (whoToStrike, collInfo.gameObject)) { 
+				DamageStruct thisisntastructanymore = new DamageStruct (damage, collider2D.gameObject, 0, 0.2f); 
+				//struct used to pass more than one parameter through send message, which only lets you pass one object as a parameter
+				collInfo.gameObject.SendMessage("callDamage",thisisntastructanymore);
+				collInfo.gameObject.SendMessage("StunYourself"); 
+				//have to manually stun target because this is a trigger and won't cause the OnCollision
+				// in the enemy class to occur. IF you try to also move the stun to to the enemy OnTrigger,
+				//a lot of bad crap will happen so please don't do that
+			}	
+	}
+}
--- Assets/Scripts/Spell Scripts/YellowSpell.cs
+++ Assets/Scripts/Spell Scripts/YellowSpell.cs
@@ -5,12 +5,14 @@ using System.Collections;
 public class YellowSpell : Spell
 {
 	GameObject yellowRefObject; //used so that spell knows what object to clone when it casts
-	
+	GameObject chargedYellowRefObject; //charged ref object
+
 	//method created to set instance variables, can't use constructor because superclass is ScriptableObject
 	public void initializeSpell(string nameDesu, string descriptionDesu, int manaCostDesu, int projSpeedDesu, float animDuration, int thechargeManaCost, int thechargeTimeRequir) 
 	{
 		base.initializeSpell(nameDesu, descriptionDesu, manaCostDesu,animDuration, thechargeManaCost, thechargeTimeRequir);
 		yellowRefObject = GameObject.FindGameObjectWithTag("YellowSpellObject");
+		chargedYellowRefObject = GameObject.FindGameObjectWithTag("ChargedYellowSpellObject");
 		projectileSpeed = projSpeedDesu;
 	}
 	
@@ -22,7 +24,27 @@ public class YellowSpell : Spell
 
 	public override void castCharge(Direction dir)
 	{
-		createProjectile (dir, yellowRefObject);
+		if (player == null){player = GameObject.FindGameObjectWithTag("Player");}
+		Vector3 clonePosition = new Vector3(0,0,0) ;
+		Vector3 cloneVelocity = new Vector3(0,0,0);
+		Quaternion cloneOrientation = Quaternion.Euler(0,0,0); 
+		if (dir == Direction.down) {
+			clonePosition = player.transform.position + new Vector3(0,5f,0);
+		}
+		else if (dir == Direction.up) {
+			clonePosition = player.transform.position + new Vector3(0,5.0f,0);
+		}
+		else if (dir == Direction.left) {
+			clonePosition = player.transform.position + new Vector3(-5.0f, 0, 0);
+		}
+		else if (dir == Direction.right) {
+			clonePosition = player.transform.position + new Vector3(5.0f,0,0);
+		}
+		//GameObject clonedesu = createSpellObject(direction, bulletToClone, clonePosition, cloneVelocity, cloneOrientation);
+		//Debug.Log(cloneVelocity);
+		GameObject clonedesu = Utilities.cloneObject(dir, chargedYellowRefObject, clonePosition, cloneVelocity, cloneOrientation);
+		Physics2D.IgnoreCollision (clonedesu.collider2D, player.collider2D);
+		Destroy (clonedesu,animationDuration/3);
 	}
 }
 
