b684506c0678c723c4a9f760426b07ce13c79775
Strakaman <straka3000@gmail.com>
2014-11-11 11:35:19 -0800

    work in progress

---
+++ Assets/NPCSpellUnlocker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCSpellUnlocker : NPCScript {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void FollowUps()
+	{
+		
+	}
+}
