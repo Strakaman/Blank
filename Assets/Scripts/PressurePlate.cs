3458a708acbd7bda0f4945fde3f57bbf7c37b997
Strakaman <straka3000@gmail.com>
2014-11-12 22:00:16 -0800

    fixed ontriggerexit not being called when object
    is destroyed

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class PressurePlate : MonoBehaviour
 {
-	public bool isActive = false;
+	private bool isActive = false;
 	public Animator animator;
+	public GameObject lastEntered; //used to check if weighted object was destroyed
 	// Use this for initialization
 	void Start ()
 	{
 			animator = (Animator)GetComponent ("Animator");
+	}
+
+	void Update()
+	{
+		if ((isActive) && (lastEntered == null)) 
+		{ //weighted object that was on pressure plate was destroyed, which doesn't trigger on exit, 
+			//so let's pseudo trigger it ourselves
+			isActive = false;
+			TriggerChildren(false); 
+		}
+
 	}
 
 	void updateAnimation ()
@@ -19,15 +31,18 @@ public class PressurePlate : MonoBehaviour
 	void OnTriggerStay2D (Collider2D trigInfo)
 	{
 		if (Utilities.hasMatchingTag("Weighted",trigInfo.gameObject)) {
-			isActive = true;
+			isActive = true; //weighted object has entered so update status and childre
 			TriggerChildren (true);
+			lastEntered = trigInfo.gameObject; 
+			//store the last entered object because if is destroyed, onTriggerExit
+			//will never be fired, so we need a pseudo onTriggerExit
 			}
 	}
 
 	void OnTriggerExit2D (Collider2D trigInfo)
 	{
 		if (Utilities.hasMatchingTag("Weighted",trigInfo.gameObject)) {
-			isActive = false;	
+			isActive = false;	//weighted object left so update status and children
 			TriggerChildren (false);
 			}
 	}
@@ -36,6 +51,7 @@ public class PressurePlate : MonoBehaviour
 	{
 
 			animator.SetBool ("isActive", trigOnTruetrigOffFalse);
-			Utilities.setStatusInChildren(transform, trigOnTruetrigOffFalse);
+			Utilities.setStatusInChildren(transform, trigOnTruetrigOffFalse); 
+			//will trigger all switchable objects in children
 	}
 }
