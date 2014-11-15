4b4eb64355c9043944b0e7653af685ac7b201762
Strakaman <straka3000@gmail.com>
2014-11-14 17:57:16 -0800

    creature spawner beginnings

---
+++ Assets/Scripts/CreatureSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureSpawner : Enemy {
+	public GameObject enemyToSpawn;
+
+	void Start()
+	{
+		enemyStart();
+		Spawn2RevengeofTheSpawner();
+	}
+	void Spawn2RevengeofTheSpawner()
+	{
+		Debug.Log("hsahshashasanfsjnlfaslkjnvsdlkjnvdslnbzfz,k,mn");
+		Instantiate(enemyToSpawn,new Vector3(transform.position.x,transform.position.y,0),Quaternion.Euler(0,0,0));
+	}
+	
+	
+}
