5d5f78d94ede039fd097fdfde51bea2391bb9188
Strakaman <straka3000@gmail.com>
2014-11-14 16:35:11 -0800

    npcs that can unlock spells and charging

---
+++ Assets/Scripts/NPCChargeUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NPCChargeUnlocker : NPCScript {
+
+	private bool followUpsCompleted = false;
+
+	public override void FollowUps()
+	{
+		if (!followUpsCompleted)
+		{
+			Utilities.TellPlayer("Spells can now be charged!");
+			PlayerInfo.SetCanCharge(true);
+			followUpsCompleted = true;
+		}
+	}
+}
---
+++ Assets/Scripts/NPCSpellUnlocker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCSpellUnlocker : NPCScript {
+	public string spellToUnlock;
+	bool followUpsCompleted = false;
+	// Use this for initialization
+
+	public override void FollowUps()
+	{
+		if (!followUpsCompleted)
+		{
+			if (spellToUnlock == null) { return;}
+			spellToUnlock = spellToUnlock.ToUpper();
+			string realSpellName="";
+			if (spellToUnlock.Contains("BLUE"))
+			{
+				realSpellName = SpellBook.BLUESPELLNAME;
+			}
+			else if (spellToUnlock.Contains("YELLOW"))
+			{
+				realSpellName = SpellBook.YELLOWSPELLNAME;
+			}
+			else if (spellToUnlock.Contains("WHITE"))
+			{
+				realSpellName = SpellBook.WHITESPELLNAME;
+			}
+			SpellBook.findSpellbyName(realSpellName).unlockSpell();
+			Utilities.TellPlayer("New Spell Unlocked!");
+			followUpsCompleted = true;
+		}
+	}
+}
