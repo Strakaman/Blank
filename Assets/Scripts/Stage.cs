a83387a9402c68170167a0cc235db70e35ff6050
Strakaman <straka3000@gmail.com>
2014-12-22 00:06:39 -0800

    bgm now changes per stage

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class Stage
 {
-	public static string LEVEL1NAME = "Kids In America";
-	public static string LEVEL2NAME = "All Star";
-	public static string LEVEL3NAME = "One Week";
-	public static string BOSSLEVELS = "Change It To Power";
-	public static string THECREDITS = "Here We Go";
+	public static string LEVEL1NAME = "Red Stage";
+	public static string LEVEL2NAME = "Blue Stage";
+	public static string LEVEL3NAME = "Yellow Stage";
+	public static string BOSSLEVELS = "Boss Room";
+	public static string THECREDITS = "End Screen";
 	public static string THEOPINTRO = "Digital Rap";
 }
 
