98085fe621ab75fadaa071c90c09eed24b2e258e
Strakaman <straka3000@gmail.com>
2015-01-04 12:46:06 -0800

    credits for now and minor tweaks

--- Assets/Main Menu/Scripts/MainMenu.cs
+++ Assets/Main Menu/Scripts/MainMenu.cs
@@ -12,41 +12,44 @@ public class MainMenu : MonoBehaviour
 
 	void Start ()
 	{
-		/*options [0] = GUILayout.Width (Screen.width * .25f);
-		options [1] = GUILayout.Width (Screen.height * .25f);
-		options [2] = GUILayout.Width (Screen.width * .25f);
-		options [3] = GUILayout.Width (Screen.height * .25f);
-		options [4] = GUILayout.Width (Screen.width * .25f);
-		options [5] = GUILayout.Width (Screen.height * .25f);
-		options [6] = GUILayout.Width (Screen.width * .25f);
-		options [7] = GUILayout.Width (Screen.height * .25f);*/
+			/*options [0] = GUILayout.Width (Screen.width * .25f);
+	options [1] = GUILayout.Width (Screen.height * .25f);
+	options [2] = GUILayout.Width (Screen.width * .25f);
+	options [3] = GUILayout.Width (Screen.height * .25f);
+	options [4] = GUILayout.Width (Screen.width * .25f);
+	options [5] = GUILayout.Width (Screen.height * .25f);
+	options [6] = GUILayout.Width (Screen.width * .25f);
+	options [7] = GUILayout.Width (Screen.height * .25f);*/
 	}
 
 	void OnGUI ()
 	{
-
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
-		if (!showYesNoPrompt)
-		{if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .4f, Screen.width * .2f, Screen.height * .1f), "New Game",oppaFontStyle)) {
-					//print ("Clicked Play Game");
-					Application.LoadLevel (Application.loadedLevel + 1); //theoretically this should be the first level
-			}
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+			if (!showYesNoPrompt) {
+					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .35f, Screen.width * .2f, Screen.height * .1f), "New Game", oppaFontStyle)) {
+							//print ("Clicked Play Game");
+							Application.LoadLevel (Application.loadedLevel + 1); //theoretically this should be the first level
+					}
+					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .5f, Screen.width * .2f, Screen.height * .1f), "Credits", oppaFontStyle)) {
+				//print ("Clicked Play Game");
+							Application.LoadLevel ("Credits"); //go to credits
+				}
 //(new Rect (Screen.width * .5f, Screen.height * .55f, Screen.width * .5f, Screen.height * .1f)
-		if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .6f, Screen.width * .2f, Screen.height * .1f), "End Game",oppaFontStyle)) {
-					//print ("Clicked End Game");
-					showYesNoPrompt = true;
+					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .65f, Screen.width * .2f, Screen.height * .1f), "End Game", oppaFontStyle)) {
+							//print ("Clicked End Game");
+							showYesNoPrompt = true;
+					}
 			}
-		}
 			if (showYesNoPrompt == true) {
-			//GUI.contentColor = Color.black; //USELESS
-					GUI.Label (new Rect (Screen.width * .45f, Screen.height * .275f, Screen.width * .4f, Screen.height * .1f), "Are you sure you want to quit?",promptStyle);
-			if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .4f, Screen.width * .16f, Screen.height * .1f), "I Guess",oppaFontStyle)) {
+					//GUI.contentColor = Color.black; //USELESS
+					GUI.Label (new Rect (Screen.width * .45f, Screen.height * .275f, Screen.width * .4f, Screen.height * .1f), "Are you sure you want to quit?", promptStyle);
+					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .4f, Screen.width * .16f, Screen.height * .1f), "I Guess", oppaFontStyle)) {
 							Application.Quit ();
 					}
-					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .6f, Screen.width * .16f, Screen.height * .1f), "No",oppaFontStyle)) {
-						showYesNoPrompt = false;
+					if (GUI.Button (new Rect (Screen.width * .575f, Screen.height * .6f, Screen.width * .16f, Screen.height * .1f), "No", oppaFontStyle)) {
+							showYesNoPrompt = false;
 					}
 			}
-		}
 	}
+}
 
---
+++ Assets/Scripts/CreditsDesu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreditsDesu : MonoBehaviour {
+
+	Dictionary<string, string[]> credits; //holds credit info
+	public Texture2D background; 
+	List<string> keys; 
+	string[] currNames;
+	int index;
+	public GUIStyle creditWordStyle; //font styles for display set in insppector
+	public GUIStyle creditWord;
+	public GUIStyle creditWord2;
+	void Start () {
+		credits = new Dictionary<string, string[]>()
+		{
+			{ "Blank Design Team" , new string[]{"Peter Pham", "Omari Straker", "Ceci Tran", "Jacob Day", "Wilson Loi", "Dennis Hsu"}},
+			{ "Director" , new string[]{"Peter Pham"}},
+			{ "Lead Programmer" , new string[]{"Omari Straker"}},
+			{ "Additional Thanks" , new string[]{"SJSU Game Dev Club","Megaman X4 Sprites"}},
+		};
+		keys = new List<string>(credits.Keys); //store keys in a seperate array for easier reference
+		index = 0; //use index to keep track of which part of credits we are displaying
+		currNames = credits[keys[index]];
+		StartCoroutine(UpdateDisplay()); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		/*
+		 * Draws the texture and text to screen based on the current value of index
+		 * since dictionary stores a string array, loop through array to print full credits per section
+		 */ 
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),background);
+		GUI.Label(new Rect (Screen.width/2-50,10,100,50),"Credits",creditWord);
+		string currKey = keys[index];
+		GUI.Label(new Rect (Screen.width/2-50,60,100,50),currKey,creditWord2);
+		//string[] currNames = credits[currKey];
+		currNames = credits[currKey];
+		for (int i = 0; i < currNames.Length; i++) {
+			GUI.Label(new Rect (Screen.width/2-50,100+i*25,100,50),currNames[i],creditWordStyle);
+		}
+	}
+	/**
+	 * Since we draw the credit info based on the value of the index counter,
+	 * we increment the value of index every few seconds, thus promptng a redraw
+	 * delay between updates dependent on length of string array to be drawn
+	 */ 
+	IEnumerator UpdateDisplay(){
+		index = 0;
+		while(index < keys.Count) {
+			yield return new WaitForSeconds(1.0f + currNames.Length*.65f);
+			index++;
+		}
+		index = keys.Count - 1; //at this point the while loop is over, just show the last credit
+		Invoke("GoToMainMenu",1); //after credits finish go back to main menu
+	}
+
+	void GoToMainMenu()
+	{
+		Application.LoadLevel("Main Menu");
+	}
+}
